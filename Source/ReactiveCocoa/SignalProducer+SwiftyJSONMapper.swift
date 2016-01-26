cbd96107a9c2e2b6d3ed00577b44f6ffa75314f4
Antoine van der Lee <a.vanderlee@triple-it.nl>
2016-01-26 21:13:08 +0100

    Extensions for RxSwift & ReactiveCocoa updated

--- Source/ReactiveCocoa/SignalProducer+SwiftyJSONMapper.swift
+++ Source/ReactiveCocoa/SignalProducer+SwiftyJSONMapper.swift
@@ -12,29 +12,28 @@ import SwiftyJSON
 /// Extension for processing Responses into Mappable objects through ObjectMapper
 extension SignalProducerType where Value == Moya.Response, Error == Moya.Error {
 
-//  /// Maps data received from the signal into an object which implements the Mappable protocol.
-//  /// If the conversion fails, the signal errors.
-//  public func mapObject<T: Mappable>(type: T.Type) -> SignalProducer<T, Error> {
-//    return producer.flatMap(.Latest) { response -> SignalProducer<T, Error> in
-//      return unwrapThrowable { try response.mapObject() }
-//    }
-//  }
-//
-//  /// Maps data received from the signal into an array of objects which implement the Mappable
-//  /// protocol.
-//  /// If the conversion fails, the signal errors.
-//  public func mapArray<T: Mappable>(type: T.Type) -> SignalProducer<[T], Error> {
-//    return producer.flatMap(.Latest) { response -> SignalProducer<[T], Error> in
-//      return unwrapThrowable { try response.mapArray() }
-//    }
-//  }
+    /// Maps data received from the signal into an object which implements the ALSwiftyJSONAble protocol.
+    /// If the conversion fails, the signal errors.
+    public func mapObject<T: ALSwiftyJSONAble>(type: T.Type) -> SignalProducer<T, Error> {
+        return producer.flatMap(.Latest) { response -> SignalProducer<T, Error> in
+            return unwrapThrowable { try response.mapObject() }
+        }
+    }
+
+    /// Maps data received from the signal into an array of objects which implement the ALSwiftyJSONAble protocol.
+    /// If the conversion fails, the signal errors.
+    public func mapArray<T: ALSwiftyJSONAble>(type: T.Type) -> SignalProducer<[T], Error> {
+        return producer.flatMap(.Latest) { response -> SignalProducer<[T], Error> in
+            return unwrapThrowable { try response.mapArray() }
+        }
+    }
 }
 
 /// Maps throwable to SignalProducer
 private func unwrapThrowable<T>(throwable: () throws -> T) -> SignalProducer<T, Moya.Error> {
-  do {
-    return SignalProducer(value: try throwable())
-  } catch {
-    return SignalProducer(error: error as! Moya.Error)
-  }
+    do {
+        return SignalProducer(value: try throwable())
+    } catch {
+        return SignalProducer(error: error as! Moya.Error)
+    }
 }
