e41523ddc453e69baabc4e3aab699524ec1cd4da
iida-hayato <hayato.iida.0213@gmail.com>
2016-09-12 16:16:23 +0900

    rename "Moya-SwiftyJSONMapper" to "MoyaSwiftyJSONMapper"

--- Moya-SwiftyJSONMapper/Moya-SwiftyJSONMapper.h
+++ Moya-SwiftyJSONMapper/Moya-SwiftyJSONMapper.h
@@ -1,6 +1,6 @@
 //
-//  Moya-SwiftyJSONMapper.h
-//  Moya-SwiftyJSONMapper
+//  MoyaSwiftyJSONMapper.h
+//  MoyaSwiftyJSONMapper
 //
 //  Created by hayato.iida on 2016/09/12.
 //  Copyright © 2016年 hayato.iida. All rights reserved.
@@ -8,10 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for Moya-SwiftyJSONMapper.
+//! Project version number for MoyaSwiftyJSONMapper.
 FOUNDATION_EXPORT double Moya_SwiftyJSONMapperVersionNumber;
 
-//! Project version string for Moya-SwiftyJSONMapper.
+//! Project version string for MoyaSwiftyJSONMapper.
 FOUNDATION_EXPORT const unsigned char Moya_SwiftyJSONMapperVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Moya_SwiftyJSONMapper/PublicHeader.h>
--- Moya-SwiftyJSONMapperTests/Moya_SwiftyJSONMapperTests.swift
+++ Moya-SwiftyJSONMapperTests/Moya_SwiftyJSONMapperTests.swift
@@ -1,36 +1,36 @@
 //
 //  Moya_SwiftyJSONMapperTests.swift
-//  Moya-SwiftyJSONMapperTests
+//  MoyaSwiftyJSONMapperTests
 //
 //  Created by hayato.iida on 2016/09/12.
 //  Copyright © 2016年 hayato.iida. All rights reserved.
 //
 
 import XCTest
-@testable import Moya_SwiftyJSONMapper
+@testable import MoyaSwiftyJSONMapper
 
 class Moya_SwiftyJSONMapperTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
