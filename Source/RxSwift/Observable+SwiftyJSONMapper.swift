4aceb7c9bf3e118f91f8f740040d86ee23b77303
davidlondono <davidlondono@users.noreply.github.com>
2016-10-08 11:02:40 -0500

    Set the path to search inside the json

--- Source/RxSwift/Observable+SwiftyJSONMapper.swift
+++ Source/RxSwift/Observable+SwiftyJSONMapper.swift
@@ -15,17 +15,23 @@ public extension ObservableType where E == Response {
 
     /// Maps data received from the signal into an object which implements the ALSwiftyJSONAble protocol.
     /// If the conversion fails, the signal errors.
-    public func mapObject<T: ALSwiftyJSONAble>(type: T.Type) -> Observable<T> {
+    public func mapObject<T: ALSwiftyJSONAble>(type: T.Type, path: [JSONSubscriptType] = []) -> Observable<T> {
         return flatMap { response -> Observable<T> in
-            return Observable.just(try response.mapObject(type: type))
+            return Observable.just(try response.mapObject(type: type, path: path))
         }
     }
-
+    public func mapObject<T: ALSwiftyJSONAble>(type: T.Type, path: JSONSubscriptType) -> Observable<T> {
+        return self.mapObject(type: type, path: [path])
+    }
+    
     /// Maps data received from the signal into an array of objects which implement the ALSwiftyJSONAble protocol.
     /// If the conversion fails, the signal errors.
-    public func mapArray<T: ALSwiftyJSONAble>(type: T.Type) -> Observable<[T]> {
+    public func mapArray<T: ALSwiftyJSONAble>(type: T.Type, path: [JSONSubscriptType] = []) -> Observable<[T]> {
         return flatMap { response -> Observable<[T]> in
-            return Observable.just(try response.mapArray(type: type))
+            return Observable.just(try response.mapArray(type: type, path: path))
         }
+    }
+    public func mapArray<T: ALSwiftyJSONAble>(type: T.Type, path: JSONSubscriptType) -> Observable<[T]> {
+        return self.mapArray(type: type, path: [path])
     }
 }
